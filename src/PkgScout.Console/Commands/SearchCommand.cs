074e5b4bf9f74abb0d12f49373c39016380afcbf
james <james_d02@protonmail.com>
2025-03-13 11:57:54 +0000

    fixes

--- src/PkgScout.Console/Commands/SearchCommand.cs
+++ src/PkgScout.Console/Commands/SearchCommand.cs
@@ -1,19 +1,52 @@
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using Microsoft.Extensions.Logging;
 using PkgScout.Shared;
 using PkgScout.Shared.Filesystem;
 using Spectre.Console.Cli;
 
 namespace PkgScout.Console.Commands;
 
-public sealed class SearchCommand(IEnumerable<IDetector> detectors)
+public sealed class SearchCommand(IEnumerable<IDetector> detectors, ILogger<SearchCommand> logger)
     : Command<SearchCommandSettings>
 {
     public override int Execute(CommandContext context, SearchCommandSettings settings)
     {
-        var files = FileScanner.GetFiles(settings.Directory);
+        var stopWatch = new Stopwatch();
+        stopWatch.Start();
+        
+        if (!Directory.Exists(settings.SearchDirectory))
+        {
+            logger.LogError("Search directory: {Directory} does not exist.", settings.SearchDirectory);
+            return 1;
+        }
 
-        var packages = detectors.SelectMany(detector => detector.Start(files));
+        if (!Directory.Exists(settings.OutputDirectory))
+        {
+            logger.LogError("Output directory: {Directory} does not exist.", settings.OutputDirectory);
+            return 1;
+        }
 
-        JsonFileWriter.WriteToFile("/home/james/Downloads/Packages.json", packages);
+        var files = FileScanner.GetFiles(settings.SearchDirectory);
+
+        var packages = new ConcurrentBag<Package>();
+
+        Parallel.ForEach(detectors, detector =>
+        {
+            var detectedPackages = detector.Start(files).ToList();
+
+            foreach (var package in detectedPackages)
+            {
+                packages.Add(package);
+            }
+        });
+
+        JsonFileWriter.WriteToFile($"{settings.OutputDirectory}/pkgscout-packages.json", packages);
+        
+        stopWatch.Stop();
+        var milliseconds = stopWatch.Elapsed.TotalMilliseconds;
+
+        logger.LogInformation("PkgScout took: {Milliseconds} ms", milliseconds);
 
         return 0;
     }
--- src/PkgScout.Console/Commands/SearchCommandSettings.cs
+++ src/PkgScout.Console/Commands/SearchCommandSettings.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel;
 using Spectre.Console.Cli;
 
 namespace PkgScout.Console.Commands;
 
 public sealed class SearchCommandSettings : CommandSettings
 {
-    [CommandArgument(0, "<Directory>")]
-    public required string Directory { get; set; }
+    [CommandOption("--search-directory <SearchDirectory>")]
+    [Description("The search directory to search within.")]
+    public required string SearchDirectory { get; set; }
+
+    [CommandOption("--output-directory <OutputDirectory>")]
+    [Description("The output directory to export the list of packages to.")]
+    public required string OutputDirectory { get; set; }
 }
